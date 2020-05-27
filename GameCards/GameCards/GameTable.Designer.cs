925a40869b5e05fb7d4df1cead5d9afc8fe436de
Andrew Polous <s13shok@gmail.com>
2020-05-28 02:57:49 +0300

    Added GameCardsApp

---
+++ GameCards/GameCards/GameTable.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace GameCards
+{
+    partial class GameTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameTable));
+            this.cards = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // cards
+            // 
+            this.cards.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("cards.ImageStream")));
+            this.cards.TransparentColor = System.Drawing.Color.Transparent;
+            this.cards.Images.SetKeyName(0, "purple_back.png");
+            this.cards.Images.SetKeyName(1, "6C.png");
+            this.cards.Images.SetKeyName(2, "6D.png");
+            this.cards.Images.SetKeyName(3, "6H.png");
+            this.cards.Images.SetKeyName(4, "6S.png");
+            this.cards.Images.SetKeyName(5, "7C.png");
+            this.cards.Images.SetKeyName(6, "7D.png");
+            this.cards.Images.SetKeyName(7, "7H.png");
+            this.cards.Images.SetKeyName(8, "7S.png");
+            this.cards.Images.SetKeyName(9, "8C.png");
+            this.cards.Images.SetKeyName(10, "8D.png");
+            this.cards.Images.SetKeyName(11, "8H.png");
+            this.cards.Images.SetKeyName(12, "8S.png");
+            this.cards.Images.SetKeyName(13, "9C.png");
+            this.cards.Images.SetKeyName(14, "9D.png");
+            this.cards.Images.SetKeyName(15, "9H.png");
+            this.cards.Images.SetKeyName(16, "9S.png");
+            this.cards.Images.SetKeyName(17, "10C.png");
+            this.cards.Images.SetKeyName(18, "10D.png");
+            this.cards.Images.SetKeyName(19, "10H.png");
+            this.cards.Images.SetKeyName(20, "10S.png");
+            this.cards.Images.SetKeyName(21, "AC.png");
+            this.cards.Images.SetKeyName(22, "AD.png");
+            this.cards.Images.SetKeyName(23, "AH.png");
+            this.cards.Images.SetKeyName(24, "AS.png");
+            this.cards.Images.SetKeyName(25, "JC.png");
+            this.cards.Images.SetKeyName(26, "JD.png");
+            this.cards.Images.SetKeyName(27, "JH.png");
+            this.cards.Images.SetKeyName(28, "JS.png");
+            this.cards.Images.SetKeyName(29, "KC.png");
+            this.cards.Images.SetKeyName(30, "KD.png");
+            this.cards.Images.SetKeyName(31, "KH.png");
+            this.cards.Images.SetKeyName(32, "KS.png");
+            this.cards.Images.SetKeyName(33, "QC.png");
+            this.cards.Images.SetKeyName(34, "QD.png");
+            this.cards.Images.SetKeyName(35, "QH.png");
+            this.cards.Images.SetKeyName(36, "QS.png");
+            // 
+            // GameTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(855, 454);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "GameTable";
+            this.Text = "Game table with cards";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.GameTable_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GameTable_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GameTable_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.GameTable_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.GameTable_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList cards;
+    }
+}
+
---
+++ GameCards/GameCards/GameTable.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace GameCards
+{
+    public partial class GameTable : Form
+    {
+        public GameTable()
+        {
+            InitializeComponent();
+        }
+
+        bool isDown = false;
+        Bitmap table;
+        Bitmap[] cardsBit = new Bitmap[36];
+        int cnt = 1;
+
+        private void DrawElements()
+        {
+            var graphics = CreateGraphics();
+            var buffGraphics = Graphics.FromImage(table);
+            buffGraphics.FillRectangle(new LinearGradientBrush(new Point(0, 0), new Point(ClientRectangle.Width, ClientRectangle.Height), Color.DarkKhaki, Color.LimeGreen), ClientRectangle);
+            buffGraphics.DrawImage(new Bitmap(cards.Images[0]), 40, 70);
+            buffGraphics.Dispose();
+            graphics.DrawImage(table, 0, 0);
+        }
+
+        private void GameTable_Load(object sender, EventArgs e)
+        {
+            table = new Bitmap(ClientRectangle.Width, ClientRectangle.Height);
+            DrawElements();
+            BackgroundImage = table;
+            table = new Bitmap(BackgroundImage);
+            for (int i = 0; i < cardsBit.Length; i++)
+            {
+                cardsBit[i] = new Bitmap(cards.Images[i + 1]);
+            }
+            //Shuffle the deck
+            var rand = new Random();
+            for (int i = cardsBit.Length - 1; i > 0; i--)
+            {
+                int j = rand.Next(i);
+                var card = cardsBit[i];
+                cardsBit[i] = cardsBit[j];
+                cardsBit[j] = card;
+            }
+        }
+
+        private void GameTable_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ((e.Location.X > 40 && e.Location.Y > 70) && (e.Location.X < 220 && e.Location.Y < 326))
+            {
+                isDown = true;
+            }
+            else if(e.Location.X < 40 && e.Location.Y < 70)
+            {
+                MessageBox.Show("Ⓒs13shok - https://github.com/s13shok");
+                System.Diagnostics.Process.Start("https://github.com/s13shok/Informatics/tree/master/GameCards");
+            }
+        }
+
+        private void GameTable_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && isDown)
+            {
+                GameTable_MouseDown(sender, e);
+                var graphics = CreateGraphics();
+                var buffGr = Graphics.FromImage(table);
+                buffGr.DrawImage(cards.Images[0], MousePosition.X - 100, MousePosition.Y - 100);
+                buffGr.Dispose();
+                graphics.DrawImage(table, 0, 0);
+                table = new Bitmap(BackgroundImage);
+            }
+        }
+
+        private void GameTable_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (cnt == 36)
+            {
+                MessageBox.Show("Карты закончились. Для обновления стола нажмите F2");
+            }
+            else if (isDown)
+            {
+                isDown = false;
+                var graphics = CreateGraphics();
+                var buffGr = Graphics.FromImage(BackgroundImage);
+                buffGr.TranslateTransform(cardsBit[cnt].Width, cardsBit[cnt].Height);
+                var rnd = new Random();
+                buffGr.RotateTransform(rnd.Next(-30, 30));
+                buffGr.DrawImage(cardsBit[cnt], MousePosition.X - 350, MousePosition.Y - 350);
+                buffGr.Dispose();
+                graphics.DrawImage(BackgroundImage, 0, 0);
+                table = new Bitmap(BackgroundImage);
+                cnt++;
+            }
+        }
+
+        private void GameTable_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F2)
+            {
+                cnt = 1;
+                DrawElements();
+                BackgroundImage = table;
+                table = new Bitmap(BackgroundImage);
+            }
+        }
+    }
+}
